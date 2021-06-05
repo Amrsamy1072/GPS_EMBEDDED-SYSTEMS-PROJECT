0fb18f15debf383db57a3caf28a750b3e4c3f9be
mohamedkhaled127 <72905885+mohamedkhaled127@users.noreply.github.com>
2021-06-05 19:34:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,30 @@ void CLK(){
 
 
 
+//uart initilization
+void uart(void){
+	SYSCTL_RCGCUART_R |= 0x04; //activate UART2
+	SYSCTL_RCGCGPIO_R |= 0x08; //activate PORTD
+	UART2_CTL_R &= ~0x01; //disable the UART
+	//BaudRate = 9600 --> 16MHZ /(16*9600) = 104.1667
+	UART2_IBRD_R = 104; //integer part = 104
+	UART2_FBRD_R = 11; //fractional part = 0.1667*64 = 11
+	UART2_LCRH_R = 0x70; //8-bit data, no parity, one stop, FIFo.s enabled
+	UART2_CTL_R |= 0x01;	//enable the UART
+	UART2_CTL_R |= 0x200; to make uart receiver
+	
+	GPIO_PORTD_AFSEL_R |= 0xC0; //enable alternate function for PD6 & PD7
+	GPIO_PORTD_DEN_R |= 0xC0; //configure PD6 & PD7 as digital I/O
+	GPIO_PORTD_PCTL_R = 0x11000000; //configure PD6 & PD7 as Tx & Rx of UART1
+	GPIO_PORTD_AMSEL_R &= ~0xC0; //disable analog on PD6 & PD7	
+ 	
+}
+char UART_Receiver(void)  
+{
+	while((UART2_FR_R&0X10)!=0){};		//to check if receiver fifo is empty or full, if empty wait for the comming data
+		 //it should be the data that we are going to process
+		return (UART2_DR_R&0xFF);
+}
 
 
 
