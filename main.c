48f87d84784087c1b95e4eee76a677f00c028ab8
Khaled amr ismail <72163567+Vnspec2@users.noreply.github.com>
2021-06-05 13:35:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -17,3 +17,11 @@
 double Deg_Rad(double deg){
     return (deg * PI / 180);
 }
+void LCD_DATA(unsigned char data)
+{
+    GPIO_PORTA_DATA_R|=0x20;           // RS=1
+    GPIO_PORTB_DATA_R=data;
+    GPIO_PORTA_DATA_R|=0x80;           // EN = HIGH
+    D_MICRO(0);
+    GPIO_PORTA_DATA_R=0x00;            // EN = LOW
+}
