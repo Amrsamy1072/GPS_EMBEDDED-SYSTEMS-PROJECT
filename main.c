80fdaed272f6fadc698ad3a4c6b156f1edcbd6fc
Kariiimehab <85250442+Kariiimehab@users.noreply.github.com>
2021-06-05 21:13:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,16 @@ void CLK(){
      while((SYSCTL_PRGPIO_R & 0x23) == 0){};    // Wait until clock is stable
 }
 
+void PORTF_INIT(){
+     // PORT F INITIALIZATION for RED LED
+     GPIO_PORTF_LOCK_R = UNLOCK_PORT;           // Unlock PORTF
+     GPIO_PORTF_CR_R |= 0x02;                   // Allow changes to PF1 (RED COLOR)
+     GPIO_PORTF_AMSEL_R &= ~0x02;               // Disable analog for PF1
+     GPIO_PORTF_AFSEL_R &= ~0x02;               // Disable alternate function for PF1
+     GPIO_PORTF_PCTL_R &= ~0X000000F0;          // Select GPIO functionality
+     GPIO_PORTF_DIR_R |= 0x02;                  // Set direction of PF1 as output
+     GPIO_PORTF_DEN_R |= 0x02;                  // Enable digital in PF1
+}
 
 
 
