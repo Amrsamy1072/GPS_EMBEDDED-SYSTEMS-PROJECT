dae76a9db00ec300c36afb70cb550000f89fcf57
Khaled amr ismail <72163567+Vnspec2@users.noreply.github.com>
2021-06-06 11:28:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -125,7 +125,12 @@ for(i=0;i<n;i++)
 for(j=0;j<3;j++)
 {}
 }
-
+void D_MILLI(int n){
+int i,j;
+for(i=0;i<n;i++)
+for(j=0;j<3180;j++)
+{}
+}
 void LCD_showdist(int value){
     int i=0;
     int arr[10];
